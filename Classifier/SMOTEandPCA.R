7aab131cac468a2ca4ee7c02240aab5339aa5c41
Shoombuatong <33542325+Shoombuatong@users.noreply.github.com>
2024-05-30 10:02:23 +0700

    Update SMOTEandPCA.R

--- Classifier/SMOTEandPCA.R
+++ Classifier/SMOTEandPCA.R
@@ -1,4 +1,11 @@
-Cdes = Descriptor
+F1 = read.csv("ESM320.csv", header = TRUE)
+F2 = read.csv("ESM480.csv", header = TRUE)
+F3 = read.csv("ESM640.csv", header = TRUE)
+F4 = read.csv("ESM1280.csv", header = TRUE)
+F5 = read.csv("ProtT5_Full.csv", header = TRUE)
+F6 = read.csv("ProtT5_BFD.csv", header = TRUE)
+
+Cdes = data.frame(F1[,-1])
 D = data.frame(Cdes, Activity = Class[,3])
 D = remove_empty(D, "cols")
 D$Activity  <- as.factor(D$Activity)
